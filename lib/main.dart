4d6691b860fae57e497adcf478a149ad94625a23
SMART24\User <hdjsjy0010002@gmail.com>
2025-03-31 12:52:23 +0900

    03-31 (2)

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hello/Setting_Page.dart';
+import 'package:hello/flex_page.dart';
 import 'package:hello/popup_page.dart';
 
 import 'drawer.dart';
@@ -24,7 +25,8 @@ class MyApp extends StatelessWidget {
       routes: {
         "/": (context) => const MyHomePage(),
         "/setting": (context) => const SettingPage(),
-        "/popup": (context) => const PopupPage()
+        "/popup": (context) => const PopupPage(),
+        "/flex": (context) => const FlexPage()
       },
     );
   }
